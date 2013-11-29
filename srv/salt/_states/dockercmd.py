b0ff644788cb7795931f412454375fd69ebb5fa3
Ming Fang <mingfang@mac.com>
2013-11-28 22:06:42 -0500

    docker fixes

--- srv/salt/_states/dockercmd.py
+++ srv/salt/_states/dockercmd.py
@@ -1,6 +1,7 @@
 import salt.utils
 from salt.exceptions import CommandExecutionError
 
+
 def __virtual__():
     '''
     Only load if docker exists on the system
@@ -9,12 +10,14 @@ def __virtual__():
         return 'dockercmd'
     return False
 
+
 retPrototype = {
-    'name':None,
-    'changes':{},
-    'result':None,
-    'comment':None
+    'name': None,
+    'changes': {},
+    'result': None,
+    'comment': None
 }
+
 
 def running(name,
             image,
@@ -27,29 +30,32 @@ def running(name,
 
     #kill existing container
     inspect = __salt__['dockercmd.inspect'](name)
-    is_running = inspect['State']['Running']
-    if is_running and not reload:
-        ret['result'] = True
-        ret['comment'] = '"%s" is already running.' % (name)
-        return ret
-    else:
-        #upper case ID is for running container
-        id = inspect['ID']
-        __salt__['dockercmd.kill'](name)
-        __salt__['dockercmd.rm'](name)
-        ret['comment'] = 'killed and removed id ' + id
+    state = inspect.get('State', {})
+    if state:
+        is_running = state.get('Running', False)
+        if is_running and not reload:
+            ret['result'] = True
+            ret['comment'] = '"%s" is already running.' % (name)
+            return ret
+        else:
+            __salt__['dockercmd.kill'](name)
+            __salt__['dockercmd.rm'](name)
+            ret['comment'] = 'killed and removed "%s" ' % (name)
 
     try:
         result = __salt__['dockercmd.run'](name, image, ports, volumes, reload)
-        ret['result'] = True
-        if isinstance(result, dict):
-            ret['changes'] = result
+        if result['retcode'] == 0:
+            ret['result'] = True
+            if isinstance(result, dict):
+                ret['changes'] = result
         else:
+            ret['result'] = False
             ret['comment'] = result
     except CommandExecutionError as ex:
         ret['result'] = False
         ret['comment'] = str(ex)
     return ret
+
 
 def built(name,
           dockerfile,
@@ -63,7 +69,7 @@ def built(name,
     inspect = __salt__['dockercmd.inspect'](name)
     if inspect['id'] and not force:
         ret['result'] = True
-        ret['comment'] = '"%s" already exist with id="%s".' % (name, inspect['id'])
+        ret['comment'] = '"%s" already built with id="%s".' % (name, inspect['id'])
         return ret
 
     result = __salt__['dockercmd.build'](name, dockerfile, nocache)
@@ -75,6 +81,7 @@ def built(name,
         ret['result'] = False
         ret['comment'] = result
     return ret
+
 
 def mod_watch(name, **kwargs):
     if kwargs['sfun'] == 'running':
